3504361fcf6254fbee7bdaeb0cb3e23a1b541ae3
Ray Cancino <cancinoray@gmail.com>
2023-03-11 22:59:16 +0800

    initial files

---
+++ client/README.md
@@ -0,0 +1,3 @@
+# Memories Project
+---
+This is a full stack javascript project using the MERN stack.
---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
+
+const CONNECTION_URL = 'mongodb+srv://memories01:memories1234@cluster0.0iwjx.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+//! I'm having an error here!
+// mongoose.set('useFindAndModify', false); 
